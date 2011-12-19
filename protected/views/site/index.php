41a0912ca3141093011e18c10759cde027192296
Alejandro Fonseca <alejandrofonseca@gmx.com>
2011-12-19 10:32:42 -0500

    change alert message to jindex

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@ Yii::app()->clientScript->registerCoreScript('jquery');
 
 Yii::app()->clientScript->registerScript('helloscript',"
         $('#login').click(function(){
-        	alert('hey!!');
+        	alert('jindex');
         });
     ",CClientScript::POS_READY);
 ?>
